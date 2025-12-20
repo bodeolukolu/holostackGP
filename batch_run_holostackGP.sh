2aaf1c97f20b871d090eee6f8ddbff7aaee6d54a
bodeolukolu <bolukolu@utk.edu>
2025-12-20 11:08:15 -0500

    use OOF/CV

--- batch_run_holostackGP.sh
+++ batch_run_holostackGP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -u pipefail
 
 export OMP_NUM_THREADS=5
 export MKL_NUM_THREADS=5
@@ -8,32 +8,43 @@ export OPENBLAS_NUM_THREADS=5
 
 max_jobs=3     # each trait is run with 5 cores when model stacking is been run, so multiply each job by 5 cores.
 count=0
+pids=()
 
 main () {
 
-declare -a arr=("trait-1" "trait-2")
-projname="proj_GP"
-phenofile="Traits.txt"
-genofile="geno.txt"
-metagenomefile="metagenome.txt"
-# set covariate to NULL, if none
-covariate="trait-10,trait-11"
-# metagenomic, genomic, holobiont, metagenomic+genomic (select only one at a time or create a nested loop below)
-kernel="genomic"
-# Additive,Dominance,metagenome or microbiome,Full (you can specify multiple e.g "Additive","Dominance")
-gene_model="Full"
+  declare -a arr=("trait-1" "trait-2")
+  projname="proj_GP"
+  phenofile="Traits.txt"
+  genofile="geno.txt"
+  metagenomefile="metagenome.txt"
+  # set covariate to NULL, if none
+  covariate="trait-10,trait-11"
+  # metagenomic, genomic, holobiont, metagenomic+genomic (select only one at a time or create a nested loop below)
+  kernel="genomic"
+  # Additive,Dominance,metagenome or microbiome,Full (you can specify multiple e.g "Additive","Dominance")
+  gene_model="Full"
 
 
-for trait in "${arr[@]}"; do
-  echo "Launching genomic prediction for trait: ${trait}"
-  Rscript batch_run_holostackGP.R "${trait}" "$projname" "$phenofile" "$genofile" "$metagenomefile" "$covariate" "$kernel" "$gene_model" &
-  sleep 10
-  ((count++))
-  if (( count % max_jobs == 0 )); then
-    wait
-  fi
-done
-wait
+  for trait in "${arr[@]}"; do
+    echo "Launching genomic prediction for trait: ${trait}"
+    Rscript batch_run_holostackGP.R "${trait}" "$projname" "$phenofile" "$genofile" "$metagenomefile" "$covariate" "$kernel" "$gene_model" &
+    sleep 10
+    pids+=($!)
+    ((count++))
+    if (( count % max_jobs == 0 )); then
+      wait
+    fi
+  done
+  wait_for_jobs
+  echo "All jobs finished"
+}
 
+wait_for_jobs() {
+  for pid in "${pids[@]}"; do
+    if ! wait "$pid"; then
+      echo "Job $pid FAILED" >&2
+    fi
+  done
+  pids=()
 }
 main > log.out 2>&1
