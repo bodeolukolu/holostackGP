15c3e6836751ee3af739a5dc0c4e7a436678bb74
bodeolukolu <bolukolu@utk.edu>
2025-12-20 23:16:29 -0500

    use OOF/CV

--- holostackGP.R
+++ holostackGP.R
@@ -657,7 +657,7 @@ holostackGP <- function(
             Y.cov <- myY[,c("Taxa",covariate)]
             Y.cov <- merge(Y.raw, Y.cov, by="Taxa")
             imp <- mice::mice(Y.cov, m = 5, method = "pmm", seed = 123)
-            Y.cov <- complete(imp, 1)   # first imputed dataset
+            Y.cov <- mice::complete(imp, 1)   # first imputed dataset
             Y.cov <- Y.cov[,c("Taxa",covariate)]
           }
 
