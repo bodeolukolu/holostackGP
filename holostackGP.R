1dadd47dfa5915e3c84d92dffcee72d8a48ef10b
bodeolukolu <bolukolu@utk.edu>
2025-12-29 18:58:43 -0500

    implement CV-aware model stacking

--- holostackGP.R
+++ holostackGP.R
@@ -843,12 +843,22 @@ holostackGP <- function(
               K_AxA <- (K_AxA + t(K_AxA)) / 2
               K_DxD <- (K_DxD + t(K_DxD)) / 2
               kernels <- list(A = K_A, D = K_D, AxD = K_AxD, AxA = K_AxA, DxD = K_DxD)
-              return(kernels)
+
+              assign("kernels", kernels, envir = .GlobalEnv)
+              # Optional: assign individual matrices as well
+              for(nm in names(kernels)) {
+                assign(nm, kernels[[nm]], envir = .GlobalEnv)
+              }
             }
             if(gp_model == "gBLUP"){
               K_M <- metagKIx
               kernels <- list(M=K_M)
-              return(kernels)
+
+              assign("kernels", kernels, envir = .GlobalEnv)
+              # Optional: assign individual matrices as well
+              for(nm in names(kernels)) {
+                assign(nm, kernels[[nm]], envir = .GlobalEnv)
+              }
             }
             if(gp_model == "gGBLUP"){
               K_M <- metagKIx
@@ -876,7 +886,12 @@ holostackGP <- function(
               K_AxDxM <- (K_AxDxM + t(K_AxDxM)) / 2
               K_M <- (K_M + t(K_M)) / 2
               kernels <- list(A = K_A, D = K_D, AxD = K_AxD, AxA = K_AxA, DxD = K_DxD, AxAxM = K_AxAxM, DxDxM = K_DxDxM, AxDxM = K_AxDxM, M = K_M)
-              return(kernels)
+
+              assign("kernels", kernels, envir = .GlobalEnv)
+              # Optional: assign individual matrices as well
+              for(nm in names(kernels)) {
+                assign(nm, kernels[[nm]], envir = .GlobalEnv)
+              }
             }
           }
 
@@ -1032,17 +1047,8 @@ holostackGP <- function(
                 ## ============================================================
                 ## Subset epistatic kernels (n × n)
                 ## ============================================================
-                train_idx <- match(train_ids, rownames(kernels[[1]]))
-                test_idx  <- match(test_ids, rownames(kernels[[1]]))
-                kernels_train <- lapply(kernels, function(K) K[train_idx, train_idx, drop = FALSE])
-                kernels_test  <- lapply(kernels, function(K) K[test_idx, train_idx, drop = FALSE])
-                #
-                # kernels_train <- lapply(kernels, function(K) {
-                #   K[train_ids, train_ids, drop = FALSE]
-                # })
-                # kernels_test <- lapply(kernels, function(K) {
-                #   K[test_ids, train_ids, drop = FALSE]
-                # })
+                kernels_train <- lapply(kernels, function(K) {K[train_ids, train_ids, drop = FALSE]})
+                kernels_test <- lapply(kernels, function(K) {K[test_ids, train_ids, drop = FALSE]})
 
                 ## ------------------------------------------------------------
                 ## Sanity checks
